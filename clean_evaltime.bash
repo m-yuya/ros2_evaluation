e3966ed8db25f13b14a593bc5ee8f56d576a87c2
m-yuya <cir9le@gmail.com>
2016-05-30 02:26:07 +0900

    Add clean_evaltime.bash

---
+++ clean_evaltime.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ./evaluation/publish_time/
+rm -f *.txt
+cd  ../subscribe_time/
+rm -f *.txt
+cd  ../transport_time/
+rm -f *.txt
+cd  ../convert_msg/
+rm -f *.txt
+cd  ../dds_time/
+rm -f *.txt
+
